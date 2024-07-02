a30bb3a881166fb4d2b51fc0d8688307b48b3a43
db developer <db.developer@gmx.de>
2024-07-02 11:05:17 +0200

    Update to version 0.1.0

--- .conf/grunt/eslint.js
+++ .conf/grunt/eslint.js
@@ -8,7 +8,7 @@
  
    return {
      options: {
-       overrideConfigFile: ".eslintrc.js"
+      overrideConfigFile: ".conf/eslint/eslint.config.js"
      },
      target: [
        GRUNTFILE,
--- src/lib/index.js
+++ src/lib/index.js
@@ -10,7 +10,7 @@
  *  index.js  is distributed WITHOUT ANY WARRANTY; without even the implied
  *  warranty  of  MERCHANTABILITY  or  FITNESS  FOR  A PARTICULAR  PURPOSE.
  *
- *//* eslint-disable-next-line */
+ */
 "use strict";
 
 /**
